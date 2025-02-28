24919bc937a42c266cd270e5f4eeec38c4319e41
Pavan Kumar Chennupati <pavankc005@gmail.com>
2025-02-28 18:00:49 +0000

    fix: changed the vite to allow the port

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server:{
+	host: '0.0.0.0',
+	post:3000,
+	allowedHosts:['askitengine.centralindia.cloudapp.azure.com']
+  }
 })
